7bc8fe2bcaa9137de34c26176a9f78d2c41bcdd7
Jetsada Machom <zinuzoid@gmail.com>
2025-11-08 00:29:48 +0000

    update

--- custom_components/aprs_weather_station/__init__.py
+++ custom_components/aprs_weather_station/__init__.py
@@ -1,8 +1,8 @@
 """
-Custom integration to integrate integration_blueprint with Home Assistant.
+Custom integration to integrate aprs_weather_station with Home Assistant.
 
 For more details about this integration, please refer to
-https://github.com/ludeeus/integration_blueprint
+https://github.com/zinuzoid/aprs_weather_station_hacs
 """
 
 from __future__ import annotations
